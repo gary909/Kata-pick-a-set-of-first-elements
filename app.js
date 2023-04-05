369e1e3626a06ee55f82c31c3f900e1d6279779f
gary909 <garywhite61@hotmail.com>
2023-04-05 13:08:06 +0200

    code working

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-function first(arr, n) {
+function first(n) {
     // return arr;
     const arr = ['a', 'b', 'c', 'd', 'e'];
+    let myArr = []
 
+    if(n == null || n == ' ' ){
+        return ['a'];
+    }
+
+    for(let i = 0; i < n; i++){
+        myArr.push(arr[i])
+    }
+    return myArr;
 }
 
-console.log(first([...arr], )); // ['a']
-console.log(first([...arr], 2)); // ['a', 'b']
+// function first(arr, n) {
+//     if(n == undefined) return arr.slice(0, 1);
+//     if(n == 0) return [];
+//     let newArr = [];
+//     if(n <= arr.length){
+//       for(let i = 0; i < n; i++){
+//         newArr.push(arr[i])
+//       } return newArr
+//     } return arr
+//   }
+
+//console.log(first([...arr], )); // ['a']
+//console.log(first([...arr], 2)); // ['a', 'b']
+console.log(first()); // ['a']
+console.log(first(2)); // ['a', 'b']
